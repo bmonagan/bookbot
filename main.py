013cda23c24aaac2d9a0a4ed6e5e783351fb6095
bmonagan <bernardmonagan@gmail.com>
2025-12-17 22:27:08 -0500

    project finished

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 from stats import get_num_words,letter_count,analyze
+
 def main():
     def get_book_text(filepath):
         with open(filepath) as f:
             file_contents = f.read()
         return file_contents
 
-    current_book = "books/frankenstein.txt"
+    user_input = sys.argv
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    current_book = user_input[1]
     book_text = get_book_text(current_book)
     
     # letter_count(book_text)
